ac8e4bf6276b648ec95417560ceb9b153b737ae2
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2022-11-11 23:42:53 +0300

    feat: implement sending value

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ const form = document.querySelector(".form");
 const input = document.querySelector(".form__input");
 const button = document.querySelector(".form__button");
 
+const SERVER_URL = "https://run.mocky.io/v3/b9efe923-cf39-4362-be17-5ad54502fd46";
+
+const sendValue = async (value) => {
+    const response = await fetch(SERVER_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            value,
+        }),
+    });
+
+    if (response.ok) alert("Запрос успешно отправлен");
+    else alert("Что-то пошло не так");
+};
+
 input.addEventListener("focus", () => {
     form.classList.remove("error");
 });
@@ -11,5 +28,7 @@ button.addEventListener("click", (event) => {
 
     if (!input.value) {
         form.classList.add("error");
+    } else {
+        sendValue(input.value);
     }
 });
