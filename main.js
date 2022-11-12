f093f7a84bef9992d421cd7dede560b1e83a7828
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2022-11-12 17:16:19 +0300

    fix: change button type, add submit event on form

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ const button = document.querySelector(".form__button");
 
 const SERVER_URL = "https://run.mocky.io/v3/b9efe923-cf39-4362-be17-5ad54502fd46";
 
+const validateForm = (value, formElement, cb) => {
+    if (!value) {
+        formElement.classList.add("error");
+    } else {
+        cb(value);
+    }
+};
+
 const sendValue = async (value) => {
     const response = await fetch(SERVER_URL, {
         method: "POST",
@@ -19,16 +27,11 @@ const sendValue = async (value) => {
     else alert("Что-то пошло не так");
 };
 
-input.addEventListener("focus", () => {
-    form.classList.remove("error");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    validateForm(input.value, form, sendValue);
 });
 
-button.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    if (!input.value) {
-        form.classList.add("error");
-    } else {
-        sendValue(input.value);
-    }
+input.addEventListener("input", () => {
+    form.classList.remove("error");
 });
